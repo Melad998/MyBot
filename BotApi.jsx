633b641ada320b7d577f73c6ad11728b72643fb9
Melad998 <118486834+Melad998@users.noreply.github.com>
2023-09-16 02:12:32 +0300

    Update BotApi.jsx
    Add api fetch to get bot telegram

--- BotApi.jsx
+++ BotApi.jsx
@@ -1,1 +1,15 @@
+import React,{useState,useRef,useEffect} from "react";
 
+
+
+
+const Botanica = (){
+    token = "6688266348:AAFU2baoOulq9YUsMyiVnEVZ8U8Iw13zPDI"
+    chat_id = 6473956939
+
+     api = fetch(`https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text={myForm}`)
+
+   return ();
+
+
+}
